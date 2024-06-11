ae94478543225dbb178c26da3896732cf536d5d9
majidied <majidimajidi2003@gmail.com>
2024-06-11 03:00:06 +0100

    Refactor import statement in 1-concurrent_coroutines.py for consistency

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,8 @@ import asyncio
 import random
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Wait for a random delay between 0 and max_delay"""
