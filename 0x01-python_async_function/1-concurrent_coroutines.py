056e8b801622d88534028ce0e18b17d1dae268eb
majidied <majidimajidi2003@gmail.com>
2024-06-11 02:59:04 +0100

    Refactor wait_n function to implement concurrent coroutines in 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,7 @@ import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Wait for a random delay between 0 and max_delay"""
     delays = [wait_random(max_delay) for i in range(n)]
