61f4850e7ccb0dee64c0776ceba3def6491373a0
majidied <majidimajidi2003@gmail.com>
2024-06-15 17:12:43 +0100

    Refactor zoom_array function with improved variable annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,7 +5,20 @@ This module contains the function zoom_array.
 from typing import Tuple, List
 
 
+from typing import List, Tuple
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Zooms in on the given list by repeating each item a certain number of times.
+
+    Args:
+        lst (Tuple): The input list to zoom in on.
+        factor (int, optional): The number of times to repeat each item. Defaults to 2.
+
+    Returns:
+        List: The zoomed-in list with repeated items.
+
+    """
     zoomed_in: List[Tuple] = [(item, i) for item in lst for i in range(factor)]
     return zoomed_in
 
