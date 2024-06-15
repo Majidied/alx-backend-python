80c9ecbb015782274850b65a7c12107de2c4c9ce
majidied <majidimajidi2003@gmail.com>
2024-06-15 16:43:56 +0100

    chore: Add sum_list function with variable annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains the function sum_list.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sums a list of floats.
+
+    Args:
+        input_list (List[float]): The list of floats to sum.
+
+    Returns:
+        float: The sum of the list of floats.
+    """
+    return sum(input_list)
