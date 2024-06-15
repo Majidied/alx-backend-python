2b923b600e18a5e01b57b65b2da91aa297a82ee0
majidied <majidimajidi2003@gmail.com>
2024-06-15 16:42:14 +0100

    Define and initialize variables with specific values

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -3,16 +3,7 @@
 This module contains the function define_variables.
 """
 
-
-def define_variables() -> None:
-    """
-    Defines and initializes variables with specific values.
-
-    Returns:
-        None
-    """
-    a: int = 1
-    pi: float = 3.14
-    i_understand_annotations: bool = True
-    school: str = "Holberton"
-    return None
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
