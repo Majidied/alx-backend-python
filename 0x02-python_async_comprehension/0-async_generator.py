db2ef39be1f989e75ca5cdd75d5dde3b9b844ff3
majidied <majidimajidi2003@gmail.com>
2024-07-05 05:46:46 +0100

    Refactor async_generator to use async for loop

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,9 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """async_generator: coroutine that yields a random number between 0 and 10"""
+    """
+    async_generator: coroutine that yields a random number between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
