07d2e0849b3ae19e51dcebfafa7576ca3c6393e2
majidied <majidimajidi2003@gmail.com>
2024-07-07 04:52:24 +0100

    Refactor test_client.py to include unit test for org method

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Test client"""
+
+import unittest
+from parameterized import parameterized
+from client import GithubOrgClient
+from unittest.mock import patch, Mock, PropertyMock
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """TestGithubOrgClient class"""
+
+    @parameterized.expand(
+        [
+            ("google"),
+            ("abc"),
+        ]
+    )
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """test_org method"""
+        test_class = GithubOrgClient(org_name)
+        test_class.org()
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
