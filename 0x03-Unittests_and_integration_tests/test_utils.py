4bd7c8feff5ef5daa7548a68552f0c27d52748e5
majidied <majidimajidi2003@gmail.com>
2024-07-07 04:47:20 +0100

    Refactor test_utils.py to include additional unit tests for access_nested_map function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,44 +10,54 @@ from unittest.mock import patch, Mock
 class TestAccessNestedMap(unittest.TestCase):
     """TestAccessNestedMap class"""
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(self, nested_map, path, expected):
         """test_access_nested_map method"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
-    ])
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError),
+        ]
+    )
     def test_access_nested_map_exception(self, nested_map, path, expected):
         """test_access_nested_map_exception method"""
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
 
+
 class TestGetJson(unittest.TestCase):
     """TestGetJson class"""
 
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
     def test_get_json(self, test_url, test_payload):
         """test_get_json method"""
-        with patch('requests.get') as mock_get:
+        with patch("requests.get") as mock_get:
             mock_get.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
+
 
 class TestMemoize(unittest.TestCase):
     """TestMemoize class"""
 
     def test_memoize(self):
         """test_memoize method"""
+
         class TestClass:
             """TestClass class"""
+
             def a_method(self):
                 """a_method method"""
                 return 42
@@ -57,7 +67,7 @@ class TestMemoize(unittest.TestCase):
                 """a_method_memoized method"""
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+        with patch.object(TestClass, "a_method", return_value=42) as mock:
             tc = TestClass()
             self.assertEqual(tc.a_property, 42)
             self.assertEqual(tc.a_property, 42)
