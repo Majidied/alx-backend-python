be79a5969e540d389243b2ba378837bcf61f8ecb
majidied <majidimajidi2003@gmail.com>
2024-07-07 05:10:25 +0100

    Refactor test_client.py to include unit test for org method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -96,3 +96,15 @@ class TestGithubOrgClient(unittest.TestCase):
             )
             mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once()
+
+    @parameterized.expand(
+        [
+            ({"license": {"key": "my_license"}}, "my_license", True),
+            ({"license": {"key": "other_license"}}, "my_license", False),
+        ]
+    )
+    def test_has_license(self, repo, license_key, expected):
+        """Tests the `has_license` method."""
+        self.assertEqual(GithubOrgClient.has_license(repo, license_key), expected)
+
+    
